3316a6be473ae178f06cd322aeda669966a8fe27
MasterUNG <phrombutr@gmail.com>
2018-07-27 07:46:36 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -20,26 +20,47 @@ class _StatefulWidget extends StatefulWidget {
     // TODO: implement createState
     return new __StatefulWidgetState();
   }
-
 }
 
 class __StatefulWidgetState extends State<_StatefulWidget> {
   @override
   Widget build(BuildContext context) {
-
     final screenSize = MediaQuery.of(context).size;
     print('Width ==> ' + screenSize.width.toString());
     print('Height ==> ' + screenSize.height.toString());
 
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text(
-            "Horizontal ListView"
+        title: new Text("Horizontal ListView"),
+      ),
+      body: new Container(
+        child: new ListView(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          children: <Widget>[
+            new Container(
+              width: screenSize.width,
+              height: screenSize.height,
+              color: Colors.red,
+              child: new Center(
+                child: new Image.asset(
+                    'images/master0.JPG'
+                ),
+              ),
+            ),
+            new Container(
+              width: screenSize.width,
+              height: screenSize.height,
+              color: Colors.blue,
+                child: new Center(
+                  child: new Image.asset(
+                      'images/master1.JPG'
+                  ),
+                ),
+            ),
+          ],
         ),
       ),
-      body: new Text("Body"),
     );
   }
 }
-
-
